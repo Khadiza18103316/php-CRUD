00f099ba3cd4e46e69f58c8a238675db6a9377b4
Khadiza <khadizaakhi756@gmail.com>
2022-01-03 00:08:49 +0600

    in tuitorial variable & array added

---
+++ tuitorial.php
@@ -0,0 +1,13 @@
+<?php
+
+$str = "hello I am Khadiza";
+$num = 5;
+$float = 7.6;
+
+echo "<h1>" . $str . "<h1>";
+echo "<h2>" . $num . "<h2>";
+echo "<h3>" . $float . "<h3>";
+
+$laptop_brand = array("HP", "Lenovo", "Asus" ,"Dell");
+echo var_dump($laptop_brand);
+?>
